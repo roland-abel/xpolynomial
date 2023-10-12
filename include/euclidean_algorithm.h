6846c0da51bdc1d8488e7b0ec093d1cdae49b606
Roland Abel <abel@adesso.de>
2023-10-12 20:37:33 +0200

    Added comments for class euclidean_algorithm

--- include/euclidean_algorithm.h
+++ include/euclidean_algorithm.h
@@ -15,7 +15,6 @@ namespace xmath {
     template<typename T>
     class euclidean_algorithm {
     public:
-
         /// @brief Computes the greatest common divisor (gcd) of two polynomials. A greatest common divisor
         /// of p and q is a polynomial d that divides p and q, and such that every common divisor of p and q
         /// also divides d.
@@ -24,9 +23,19 @@ namespace xmath {
         /// @return Returns the gcd of p and q.
         static polynomial<T> euclidean(const polynomial<T> &p, const polynomial<T> &q);
 
-        /// @brief
-        /// @param p
-        /// @param q
+        /// @brief Computes the extended Euclidean algorithm for two polynomials.
+        /// Example usage:
+        /// @code{.cpp}
+        ///
+        /// auto X = polynomial<double>::monomial(1, 1.0);
+        /// auto p = X.pow(4) - 2 * X.pow(3) - 6 * X.pow(2) + 12 * X + 15;
+        /// auto q = X.pow(3) + X.pow(2) - 4 * X - 4;
+        /// auto [s, t, g] = EuclideanAlgorithm::extended_euclidean(p, q);
+        /// s * p + t * q == g
+        ///
+        /// @endcode
+        /// @param p The first polynomial for the extended Euclidean algorithm.
+        /// @param q The second polynomial for the extended Euclidean algorithm.
         /// @return Returns polynomials s, t, g such that g = gcd(p, q) = s*p + t*q.
         static std::tuple<polynomial<T>, polynomial<T>, polynomial<T>> extended_euclidean(
                 const polynomial<T> &p,
