651ef7f974a5eeecec2bcd254612b3cbe6768fcb
Roland Abel <abel@adesso.de>
2023-10-14 11:29:17 +0200

    Fixed typo

--- test/chebyshev_polynomial_tests.cpp
+++ test/chebyshev_polynomial_tests.cpp
@@ -10,7 +10,7 @@ using namespace xmath;
 
 namespace {
     using Polynomial = polynomial<double>;
-    using ChebyshvPolynomial = chebyshev_polynomial<double>;
+    using ChebyshevPolynomial = chebyshev_polynomial<double>;
 
     constexpr auto epsilon = Polynomial::tolerance;
     auto zero = Polynomial::zero();
@@ -19,37 +19,37 @@ namespace {
 }
 
 TEST(ChebyshvPolynomialTest, FirstKindChebyshvPolynomials) {
-    auto T_0 = ChebyshvPolynomial::create_1st_kind(0);
+    auto T_0 = ChebyshevPolynomial::create_1st_kind(0);
     EXPECT_EQ(T_0, one);
 
-    auto T_1 = ChebyshvPolynomial::create_1st_kind(1);
+    auto T_1 = ChebyshevPolynomial::create_1st_kind(1);
     EXPECT_EQ(T_1, X);
 
-    auto T_2 = ChebyshvPolynomial::create_1st_kind(2);
+    auto T_2 = ChebyshevPolynomial::create_1st_kind(2);
     EXPECT_EQ(T_2, 2 * X.pow(2) - 1);
 
-    auto T_3 = ChebyshvPolynomial::create_1st_kind(3);
+    auto T_3 = ChebyshevPolynomial::create_1st_kind(3);
     EXPECT_EQ(T_3, 4 * X.pow(3) - 3 * X);
 
-    auto T_4 = ChebyshvPolynomial::create_1st_kind(4);
+    auto T_4 = ChebyshevPolynomial::create_1st_kind(4);
     EXPECT_EQ(T_4, 8 * X.pow(4) - 8 * X.pow(2) + 1);
 
-    auto T_5 = ChebyshvPolynomial::create_1st_kind(5);
+    auto T_5 = ChebyshevPolynomial::create_1st_kind(5);
     EXPECT_EQ(T_5, 16 * X.pow(5) - 20 * X.pow(3) + 5 * X);
 
-    auto T_6 = ChebyshvPolynomial::create_1st_kind(6);
+    auto T_6 = ChebyshevPolynomial::create_1st_kind(6);
     EXPECT_EQ(T_6, 32 * X.pow(6) - 48 * X.pow(4) + 18 * X.pow(2) - 1);
 
-    auto T_7 = ChebyshvPolynomial::create_1st_kind(7);
+    auto T_7 = ChebyshevPolynomial::create_1st_kind(7);
     EXPECT_EQ(T_7, 64 * X.pow(7) - 112 * X.pow(5) + 56 * X.pow(3) - 7 * X);
 
-    auto T_8 = ChebyshvPolynomial::create_1st_kind(8);
+    auto T_8 = ChebyshevPolynomial::create_1st_kind(8);
     EXPECT_EQ(T_8, 128 * X.pow(8) - 256 * X.pow(6) + 160 * X.pow(4) - 32 * X.pow(2) + 1);
 
-    auto T_9 = ChebyshvPolynomial::create_1st_kind(9);
+    auto T_9 = ChebyshevPolynomial::create_1st_kind(9);
     EXPECT_EQ(T_9, 256 * X.pow(9) - 576 * X.pow(7) + 432 * X.pow(5) - 120 * X.pow(3) + 9 * X);
 
-    auto T_10 = ChebyshvPolynomial::create_1st_kind(10);
+    auto T_10 = ChebyshevPolynomial::create_1st_kind(10);
     EXPECT_EQ(T_10, 512 * X.pow(10) - 1280 * X.pow(8) + 1120 * X.pow(6) - 400 * X.pow(4) + 50 * X.pow(2) - 1);
 }
 
