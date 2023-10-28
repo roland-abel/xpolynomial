3a92d6ac503b974508aef8ee424e802a9b21f180
Roland Abel <roland.abel@live.de>
2023-10-28 11:07:06 +0200

    Added example `nth_roots_of_unity`

---
+++ examples/nth_roots_of_unity.cpp
@@ -0,0 +1,36 @@
+/// @file nth_roots_of_unity.cpp.cpp
+/// @brief
+///
+/// @author Roland Abel
+/// @date 28.10.2023.
+
+#include <numeric>
+#include <vector>
+#include <iostream>
+#include "complex_polynomial_root_finder.h"
+
+using namespace std;
+using namespace xmath;
+
+namespace {
+    using ComplexPolynomial = complex_polynomial<double>;
+    using RealPolynomial = polynomial<double, polynomial_specification<double>>;
+    using RootFinder = complex_polynomial_root_finder<double>;
+
+    auto X = RealPolynomial::monomial(1, 1.0);
+    auto Y = RealPolynomial::monomial(1, 1.0);
+    auto Z = ComplexPolynomial::monomial(1, 1.0);
+}
+
+int main() {
+    auto n = 13;
+    auto p = Z.pow(n) - 1.;
+    auto roots = RootFinder::nth_roots_of_unity(n);
+
+    cout << "Has roots: " << p.has_roots(roots) << endl;
+    cout << n << "th-roots:" << endl;
+    for (auto z: roots) {
+        cout << z << endl;
+    }
+    return 0;
+}
