6b9934c105f554efa5f5ccb4420aa60dc7903875
Roland Abel <roland.abel@live.de>
2023-10-29 18:13:48 +0100

    Roots of the Chebyshev polynomial (1st kind)

--- include/chebyshev_polynomial.h
+++ include/chebyshev_polynomial.h
@@ -16,11 +16,18 @@ namespace xmath {
     class chebyshev_polynomial {
     public:
         using polynomial_sequence = std::vector<polynomial<T>>;
+        using value_type = polynomial<T>::value_type;
+        using values_type = polynomial<T>::values_type;
 
         /// @brief Creates first kind Chebyshev polynomial T_n for the given order.
         /// @param order The order of the polynomial.
         /// @return The Chebyshev polynomial T_n of the first kind.
         static polynomial<T> create_1st_kind(size_t order);
+
+        /// @brief
+        /// @param order
+        /// @return
+        static values_type roots_1st_kind(size_t order);
 
     private:
         static polynomial_sequence chebyshev_1st_kind_polynomials_;
