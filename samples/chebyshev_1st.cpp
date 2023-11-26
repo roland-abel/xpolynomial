a6909b469544067d2d2ebf26958ae1bccf01bc8c
Roland Abel <roland.abel@live.de>
2023-11-26 15:16:58 +0100

    Updated file headers and added licence text

--- samples/chebyshev_1st.cpp
+++ samples/chebyshev_1st.cpp
@@ -1,8 +1,33 @@
 /// @file chebyshev_1st.cpp
-/// @brief Example for Chebyshev polynomials of the 1st kind.
+/// @brief Example to demonstrate the computation of Chebyshev Polynomials of the 1st kind.
+///
+/// This program calculates and prints Chebyshev Polynomials of the 1st kind up to a specified maximum order.
+/// It uses the `chebyshev_polynomial<>` class to generate the polynomials.
 ///
 /// @author Roland Abel
-/// @date 14.10.2023
+/// @date October 14, 2023
+///
+/// Copyright (c) 2023 Roland Abel
+///
+/// This software is released under the MIT License.
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 #include "chebyshev_polynomial.h"
 
