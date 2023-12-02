4e67bffaa2a3c860a0fb8f389755a518bfb09d1a
Roland Abel <roland.abel@live.de>
2023-12-02 23:22:27 +0100

    Renamed `tolerance` -> `epsilon`

--- include/root_finder.h
+++ include/root_finder.h
@@ -46,14 +46,14 @@ namespace xmath {
         /// @brief Perform the bisection method to find a zero point of the given function within the specified real_interval.
         /// @param func The function for which the zero point needs to be found.
         /// @param I The real interval.
-        /// @param tolerance The desired accuracy for the root approximation (default is 1e-15).
+        /// @param epsilon The desired accuracy for the root approximation (default is 1e-15).
         /// @return The approximate zero point of the function within the given real_interval,
         /// or NaN if the Intermediate Value Theorem conditions are not met (The function has the same sign at both
         /// real_interval boundaries).
         static value_type bisection(
                 const std::function<value_type(value_type)> &func,
                 const real_interval<value_type> &I,
-                value_type tolerance = 1e-5);
+                value_type epsilon = 1e-15);
 
         /// @brief Perform the regula falsi method to find a zero point of the given function within the specified real_interval.
         /// @param func The function for which the zero point needs to be found.
@@ -66,14 +66,14 @@ namespace xmath {
         static value_type regula_falsi(
                 const std::function<value_type(value_type)> &func,
                 const real_interval<value_type> &I,
-                value_type epsilon = 1e-5);
+                value_type epsilon = 1e-15);
 
         /// @brief Computes an approximation of a root for an equation using the Newton-Raphson method.
         /// @param func The function for which to find the root.
         /// @param derive The derivative of the func function.
         /// @param initial The initial value for the iteration.
         /// @param max_iterations The maximum number of iterations (default: 100).
-        /// @param tolerance The tolerance for the approximation to the root (default: 1e-5).
+        /// @param epsilon The epsilon for the approximation to the root (default: 1e-15).
         /// @return The approximation of the solution for the equation.
         ///
         /// @note If no solution is found, an undefined value may be returned (NaN).
@@ -83,7 +83,7 @@ namespace xmath {
                 const std::function<value_type(const value_type &)> &derive,
                 value_type initial,
                 int max_iterations = 100,
-                value_type tolerance = 1e-5);
+                value_type epsilon = 1e-15);
     };
 }
 
